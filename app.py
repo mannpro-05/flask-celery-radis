24d7e62113db88ccac1c854f7f7252007d60d7f1
mannpro-05 <62499938+mannpro-05@users.noreply.github.com>
2022-06-09 15:01:09 -0400

    Initial commit

--- app.py
+++ app.py
@@ -1,11 +1,10 @@
 from celery import Celery, Task
+from celery.signals import after_task_publish, task_success, task_postrun, task_failure
 from flask import Flask
 
 app = Flask(__name__)
 app.config['CELERY_BROKER_URL'] = 'redis://localhost:6379/0'
 app.config['CELERY_RESULT_BACKEND'] = 'redis://localhost:6379/0'
-
-print(app.name)
 
 celery = Celery(app.name, broker=app.config['CELERY_BROKER_URL'], backend=app.config['CELERY_RESULT_BACKEND'])
 
@@ -24,14 +23,33 @@ def find_fibonacci_async(number):
     return result
 
 
+@after_task_publish.connect
+def publish(sender=None, headers=None, body=None, **kwargs):
+    print("Task Registered!", headers)
+
+
+@task_postrun.connect
+def post_run(**kwargs):
+    f = open("demofile3.txt", "w")
+    f.write("Woops! I have deleted the content!")
+    f.close()
+
+
+@task_success.connect(sender=find_fibonacci_async)
+def task_success_notifier(sender=None, **kwargs):
+    with open('text.txt', 'w') as file:
+        file.write('manasdasdsadasdasnn')
+        file.close()
+    print('hellll1ooasasdasdasdasdwqeqwe3w21233423141234123ddsqf ewfqdaskdas')
+
+
+
+
 @app.route("/")
 def hello():
-
     ans: Task = find_fibonacci_async.delay(35)
-    print(ans.status)
-    ans.wait()
-    print(ans.status, ans.result)
     return "Hello, World!"
 
+
 if __name__ == '__main__':
-    app.run()
+    app.run(host='0.0.0.0', port=5000)
